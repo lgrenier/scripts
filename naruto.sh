7cb38d1dad6a2bd12db55670fb998685d04e1a0a
Laurent GRENIER <lgrenier@G73.laurent-grenier.fr>
2016-05-08 18:58:14 +0200

    naruto S22

--- naruto.sh
+++ naruto.sh
@@ -7,7 +7,7 @@ number=$(egrep -o [0-9]+ <<<$file)
 
 #number=$(awk -F'[^0-9]*' '{print $2}' <<< $1)
 
-number=$((number - 450))
+number=$((number - 458))
 
 #number=${filename//[^0-9]/}
 
@@ -15,4 +15,4 @@ echo number trouvé $number
 echo file $file
 echo extension $extension
 
-mv "$file.$extension" "Naruto_Shippuuden.S21E$number.720p.HDTV.h264-FansubResistance.$extension"
+mv "$file.$extension" "Naruto_Shippuuden.S22E$number.720p.HDTV.h264-FansubResistance.$extension"
